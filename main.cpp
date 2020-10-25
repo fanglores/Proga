52fe184432e056f2aaa3911a2e5b1c7cfa1c6722
fanglore@joker-pc <kostiktsat@gmail.com>
2020-10-25 11:46:43 +0300

    added test function

--- main.cpp
+++ main.cpp
@@ -21,9 +21,30 @@ int division(int a, int b)
 		return a / b;
 }
 
+void test()
+{
+	int a, b, c;
+	cout << "~Testing addition~\n>>";
+	scanf("%d + %d = %d", &a, &b, &c);
+	if(addition(a,b) == c) cout << "Successful!\n\n";
+	
+	cout << "~Testing subtraction~\n>>";
+	scanf("%d - %d = %d", &a, &b, &c);
+	if(subtraction(a,b) == c) cout << "Successful!\n\n";
+	
+	cout << "~Testing multiplication~\n>>";
+	scanf("%d * %d = %d", &a, &b, &c);
+	if(multiplication(a,b) == c) cout << "Successful!\n\n";
+	
+	cout << "~Testing division~\n>>";
+	scanf("%d / %d = %d", &a, &b, &c);
+	if(division(a,b) == c) cout << "Successful!\n\n";
+
+}
+
 int main()
 {
 	
 	cout << "Hll wrld!" << endl;
-	
+	//test();
 }
